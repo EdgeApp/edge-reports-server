7b000ee541a561024e7f1f14996e08ea4b33f385
Matthew <matthew@edge.app>
2020-03-11 14:23:58 -0700

    Add comments and settings edit to fake plugin

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import CONFIG from '../config.json'
 import './types.ts'
 import fakePartnerPlugin from './fake'
 
+function getData {
+  // Loop over each partner:
+  // get settings for partner from database
+  // send settings to partner plugin
+  // send returned settings from partner plugin to database
+  // send returned transactions to database
+}
 
-
-
+getData()
