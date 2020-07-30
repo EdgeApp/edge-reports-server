a56ea3d6b4b41113df5c208953d211b7c5d799e4
Thomas Gonda <passinto06@gmail.com>
2020-07-30 01:53:20 -0700

    added cleaner to currencies and removed some unnecessary logic that checked what was in currencies later

--- src/partners/moonpay.ts
+++ src/partners/moonpay.ts
@@ -3,6 +3,13 @@ import fetch from 'node-fetch'
 
 import { PartnerPlugin, PluginParams, PluginResult, StandardTx } from '../types'
 
+const asCurrencies = asArray(
+  asObject({
+    id: asString,
+    code: asString
+  })
+)
+
 const asMoonpayTx = asObject({
   cryptoTransactionId: asString,
   baseCurrencyAmount: asNumber,
@@ -14,19 +21,11 @@ const asMoonpayTx = asObject({
   currencyId: asString
 })
 
-const asMoonpayResult = asArray(
-  asObject({
-    status: asString,
-    cryptoTransactionId: asUnknown,
-    baseCurrencyAmount: asUnknown,
-    walletAddress: asUnknown,
-    quoteCurrencyAmount: asUnknown,
-    createdAt: asUnknown,
-    id: asUnknown,
-    baseCurrencyId: asUnknown,
-    currencyId: asUnknown
-  })
-)
+const asMoonpayRawTx = asObject({
+  status: asString
+})
+
+const asMoonpayResult = asArray(asUnknown)
 
 const QUERY_LOOKBACK = 1000 * 60 * 60 * 24 * 5
 const PER_REQUEST_LIMIT = 50
@@ -38,7 +37,7 @@ export async function queryMoonpay(
   let apiKey
   let headers
   let currencies
-  let latestTimestamp = Date.now()
+  let latestTimestamp = 0
   if (typeof pluginParams.settings.latestTimestamp === 'number') {
     latestTimestamp = pluginParams.settings.latestTimestamp
   }
@@ -48,7 +47,7 @@ export async function queryMoonpay(
     try {
       const currenciesUrl = 'https://api.moonpay.io/v2/currencies'
       const currenciesResult = await fetch(currenciesUrl)
-      currencies = await currenciesResult.json()
+      currencies = asCurrencies(await currenciesResult.json())
       headers = {
         Authorization: `Api-Key ${apiKey}`
       }
@@ -80,61 +79,43 @@ export async function queryMoonpay(
     // in bulk update it throws an error for document update conflict because of this.
 
     for (const rawtx of txs) {
-      let tx
-      if (rawtx.status === 'completed') {
-        const duplicate = ssFormatTxs.find(
-          object => object.inputTXID === rawtx.cryptoTransactionId
-        )
-        if (duplicate !== undefined) {
-          console.log('DUPLICATE TRANSACTION ID FOUND')
-          console.log('DUPLICATE IN ARRAY:', JSON.stringify(duplicate))
-          console.log('CURRENT RAW TX:', JSON.stringify(rawtx))
-        }
-        tx = asMoonpayTx(rawtx)
+      if (asMoonpayRawTx(rawtx).status === 'completed') {
+        const tx = asMoonpayTx(rawtx)
+
         const baseCurrency = currencies.find(
           cur => cur.id === tx.baseCurrencyId
         )
         const outputCurrency = currencies.find(cur => cur.id === tx.currencyId)
-        const baseCurrencyCode =
-          typeof baseCurrency === 'object' &&
-          typeof baseCurrency.code === 'string'
-            ? baseCurrency.code.toUpperCase()
-            : ''
-        const outputCurrencyCode =
-          typeof outputCurrency === 'object' &&
-          typeof outputCurrency.code === 'string'
-            ? outputCurrency.code.toUpperCase()
-            : ''
-        if (baseCurrencyCode === '') {
-          console.warn(
-            `baseCurrencyCode not defined for Moonpay tx ID: ${tx.id}`
+
+        if (typeof baseCurrency === 'undefined') {
+          throw new Error(
+            `baseCurrency not defined for Moonpay tx ID: ${tx.id}`
           )
         }
-        if (outputCurrencyCode === '') {
-          console.warn(
-            `outputCurrencyCode not defined for Moonpay tx ID: ${tx.id}`
+        if (typeof outputCurrency === 'undefined') {
+          throw new Error(
+            `outputCurrency not defined for Moonpay tx ID: ${tx.id}`
           )
         }
-        if (baseCurrencyCode !== '' && outputCurrencyCode !== '') {
-          const date = new Date(tx.createdAt)
-          const timestamp = date.getTime()
-          const ssTx = {
-            status: 'complete',
-            inputTXID: tx.id,
-            inputAddress: '',
-            inputCurrency: baseCurrencyCode,
-            inputAmount: tx.baseCurrencyAmount,
-            outputAddress: tx.walletAddress,
-            outputCurrency: outputCurrencyCode,
-            outputAmount: tx.quoteCurrencyAmount,
-            timestamp,
-            isoDate: date.toISOString()
-          }
-          ssFormatTxs.push(ssTx)
-          done = latestTimestamp > timestamp || txs.length < PER_REQUEST_LIMIT
-          newestTimestamp =
-            newestTimestamp > timestamp ? newestTimestamp : timestamp
+
+        const date = new Date(tx.createdAt)
+        const timestamp = date.getTime()
+        const ssTx = {
+          status: 'complete',
+          inputTXID: tx.id,
+          inputAddress: '',
+          inputCurrency: baseCurrency.code.toUpperCase(),
+          inputAmount: tx.baseCurrencyAmount,
+          outputAddress: tx.walletAddress,
+          outputCurrency: outputCurrency.code.toUpperCase(),
+          outputAmount: tx.quoteCurrencyAmount,
+          timestamp: timestamp / 1000,
+          isoDate: date.toISOString()
         }
+        ssFormatTxs.push(ssTx)
+        done = latestTimestamp > timestamp || txs.length < PER_REQUEST_LIMIT
+        newestTimestamp =
+          newestTimestamp > timestamp ? newestTimestamp : timestamp
       }
     }
 
