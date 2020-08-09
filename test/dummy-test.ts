9d11bf3c82831a5e5f0c663dc3f91573a9acb1a0
Thomas Gonda <passinto06@gmail.com>
2020-08-08 23:31:58 -0700

    delete useless default test file

--- test/dummy-test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai'
-import { describe, it } from 'mocha'
-
-describe('edge-reports-server', function() {
-  it("Doesn't have tests yet", function() {
-    expect(1 + 1).equals(2)
-  })
-})
