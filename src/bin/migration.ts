5b07ee713a88283f9eb859329e77bee3f262063a
thomasgonda3 <passinto06@gmail.com>
2020-09-10 16:40:38 -0700

    Migration Fixes

--- src/bin/migration.ts
+++ src/bin/migration.ts
@@ -12,10 +12,10 @@ import {
 import js from 'jsonfile'
 import nano from 'nano'
 
-import config from '../../config.json'
-import { StandardTx } from '../types'
+import { DbTx, StandardTx } from '../types'
 import { datelog } from '../util'
 
+const config = js.readFileSync('./config.json')
 const banxaJSON = js.readFileSync('./cache/banRaw.json')
 const bitrefillJSON = js.readFileSync('./cache/brRaw.json')
 const bitsofgoldJSON = js.readFileSync('./cache/bogRaw.json')
@@ -36,14 +36,11 @@ const totleJSON = js.readFileSync('./cache/tlRaw.json')
 const transakJSON = js.readFileSync('./cache/tnkRaw.json')
 const wyreJSON = js.readFileSync('./cache/wyrRaw.json')
 
-const asTimestamps = asObject({
-  timestamp: asNumber
+const asPartitionTimestamp = asObject({
+  doc: asObject({
+    timestamp: asNumber
+  })
 })
-
-const asTimestampResult = asObject({
-  docs: asArray(asTimestamps)
-})
-
 const asPartner = asObject({
   name: asString,
   txs: asArray(asUnknown)
@@ -120,24 +117,32 @@ async function migration(): Promise<void> {
   for (const partnerJSON of oldTransactions) {
     const partner = asPartner(partnerJSON)
     const appAndPluginId = `edge_${partner.name}`
-    const query = {
-      selector: {
-        timestamp: { $gte: 0 }
-      },
-      fields: ['timestamp'],
-      limit: 100000000
-    }
-    const transactionTimestamps = asTimestampResult(
-      await reportsTransactions.partitionedFind(appAndPluginId, query)
-    )
+    let earliestTimestamp
 
-    let earliestTimestamp = transactionTimestamps.docs[0].timestamp
-    for (let i = 1; i < transactionTimestamps.docs.length; i++) {
-      earliestTimestamp =
-        earliestTimestamp < transactionTimestamps.docs[i].timestamp
-          ? earliestTimestamp
-          : transactionTimestamps.docs[i].timestamp
+    try {
+      await reportsTransactions
+        .partitionedList(appAndPluginId, { include_docs: true })
+        .then(body => {
+          body.rows.forEach(doc => {
+            const timestamp = asPartitionTimestamp(doc).doc.timestamp
+            if (
+              earliestTimestamp === undefined ||
+              timestamp < earliestTimestamp
+            ) {
+              earliestTimestamp = timestamp
+            }
+          })
+        })
+    } catch (e) {
+      datelog(e)
+      throw e
     }
+
+    if (earliestTimestamp === undefined) {
+      datelog(`Partition ${appAndPluginId} does not exist.`)
+      earliestTimestamp = 9999999999
+    }
+
     const earliestDate = new Date(earliestTimestamp * 1000).toISOString()
     const filteredTransactions = partner.txs.filter(obj => {
       const checkObj = asRawTx(obj)
@@ -149,95 +154,77 @@ async function migration(): Promise<void> {
         return obj
       }
     })
-    const reformattedTxs = filteredTransactions.map(tx => {
-      if (appAndPluginId === `edge_shapeshift`) {
-        const cleanedShapeshiftTx = asShapeshiftTx(tx)
-        return {
-          status: 'complete',
-          orderId: cleanedShapeshiftTx.orderId,
-          depositTxid: cleanedShapeshiftTx.inputTXID,
-          depositAddress: cleanedShapeshiftTx.inputAddress,
-          depositCurrency: cleanedShapeshiftTx.inputCurrency,
-          depositAmount: cleanedShapeshiftTx.inputAmount,
-          payoutTxid: cleanedShapeshiftTx.outputTXID,
-          payoutAddress: cleanedShapeshiftTx.outputAddress,
-          payoutCurrency: cleanedShapeshiftTx.outputCurrency,
-          payoutAmount: parseFloat(cleanedShapeshiftTx.outputAmount),
-          timestamp: cleanedShapeshiftTx.timestamp,
-          isoDate: new Date(cleanedShapeshiftTx.timestamp * 1000).toISOString(),
-          usdValue: undefined,
-          rawTx: tx
+    const reformattedTxs: DbTx[] = await Promise.all(
+      filteredTransactions.map(async tx => {
+        if (appAndPluginId === `edge_shapeshift`) {
+          const cleanedShapeshiftTx = asShapeshiftTx(tx)
+          const newTx = {
+            status: 'complete',
+            orderId: cleanedShapeshiftTx.orderId,
+            depositTxid: cleanedShapeshiftTx.inputTXID,
+            depositAddress: cleanedShapeshiftTx.inputAddress,
+            depositCurrency: cleanedShapeshiftTx.inputCurrency,
+            depositAmount: cleanedShapeshiftTx.inputAmount,
+            payoutTxid: cleanedShapeshiftTx.outputTXID,
+            payoutAddress: cleanedShapeshiftTx.outputAddress,
+            payoutCurrency: cleanedShapeshiftTx.outputCurrency,
+            payoutAmount: parseFloat(cleanedShapeshiftTx.outputAmount),
+            timestamp: cleanedShapeshiftTx.timestamp,
+            isoDate: new Date(
+              cleanedShapeshiftTx.timestamp * 1000
+            ).toISOString(),
+            usdValue: undefined,
+            rawTx: tx
+          }
+          return standardTxReformat(newTx, appAndPluginId, reportsTransactions)
         }
-      }
-      const cleanedOldTx = asOldTx(tx)
-      const timestamp =
-        typeof cleanedOldTx.timestamp === 'number'
-          ? cleanedOldTx.timestamp
-          : parseInt(cleanedOldTx.timestamp)
-      const isoDate = new Date(timestamp * 1000).toISOString()
-      const depositAddress =
-        typeof cleanedOldTx.inputAddress === 'string' &&
-        cleanedOldTx.inputAddress.length > 0
-          ? cleanedOldTx.inputAddress
-          : undefined
-      const payoutAddress =
-        cleanedOldTx.outputAddress.length > 0
-          ? cleanedOldTx.outputAddress
-          : undefined
-      const depositAmount =
-        typeof cleanedOldTx.inputAmount === 'number'
-          ? cleanedOldTx.inputAmount
-          : parseFloat(cleanedOldTx.inputAmount)
-      const payoutAmount =
-        typeof cleanedOldTx.outputAmount === 'number'
-          ? cleanedOldTx.outputAmount
-          : parseFloat(cleanedOldTx.outputAmount)
-      return {
-        orderId: cleanedOldTx.inputTXID,
-        depositTxid: undefined,
-        depositAddress,
-        depositCurrency: cleanedOldTx.inputCurrency,
-        depositAmount,
-        payoutTxid: undefined,
-        payoutAddress,
-        payoutCurrency: cleanedOldTx.outputCurrency,
-        payoutAmount,
-        status: 'complete',
-        isoDate,
-        timestamp,
-        usdValue: undefined,
-        rawTx: undefined
-      }
-    })
+        const cleanedOldTx = asOldTx(tx)
+        const timestamp =
+          typeof cleanedOldTx.timestamp === 'number'
+            ? cleanedOldTx.timestamp
+            : parseFloat(cleanedOldTx.timestamp)
+        const isoDate = new Date(timestamp * 1000).toISOString()
+        const depositAddress =
+          typeof cleanedOldTx.inputAddress === 'string' &&
+          cleanedOldTx.inputAddress.length > 0
+            ? cleanedOldTx.inputAddress
+            : undefined
+        const payoutAddress =
+          cleanedOldTx.outputAddress.length > 0
+            ? cleanedOldTx.outputAddress
+            : undefined
+        const depositAmount =
+          typeof cleanedOldTx.inputAmount === 'number'
+            ? cleanedOldTx.inputAmount
+            : parseFloat(cleanedOldTx.inputAmount)
+        const payoutAmount =
+          typeof cleanedOldTx.outputAmount === 'number'
+            ? cleanedOldTx.outputAmount
+            : parseFloat(cleanedOldTx.outputAmount)
+        const newTx = {
+          orderId: cleanedOldTx.inputTXID,
+          depositTxid: undefined,
+          depositAddress,
+          depositCurrency: cleanedOldTx.inputCurrency,
+          depositAmount,
+          payoutTxid: undefined,
+          payoutAddress,
+          payoutCurrency: cleanedOldTx.outputCurrency,
+          payoutAmount,
+          status: 'complete',
+          isoDate,
+          timestamp,
+          usdValue: undefined,
+          rawTx: undefined
+        }
+        return standardTxReformat(newTx, appAndPluginId, reportsTransactions)
+      })
+    )
     datelog(
       `Importing ${filteredTransactions.length} transactions for ${partner.name} before date ${earliestDate}.`
     )
-    const transactionsArray: StandardTx[] = []
-    for (const transaction of reformattedTxs) {
-      transaction.orderId = transaction.orderId.toLowerCase()
-      const key = `${appAndPluginId}:${transaction.orderId}`.toLowerCase()
-      const result = await reportsTransactions.get(key).catch(e => {
-        if (e != null && e.error === 'not_found') {
-          return {}
-        } else {
-          throw e
-        }
-      })
-      // no duplicate transactions
-      if (Object.keys(result).length > 0) {
-        continue
-      }
-      const newObj = { _rev: undefined, ...result, ...transaction, _id: key }
-
-      // replace all fields with non-standard names
-      newObj.depositCurrency = standardizeNames(newObj.depositCurrency)
-      newObj.payoutCurrency = standardizeNames(newObj.payoutCurrency)
-
-      datelog(`id: ${newObj._id}. revision: ${newObj._rev}`)
-      transactionsArray.push(newObj)
-    }
     try {
-      const docs = await reportsTransactions.bulk({ docs: transactionsArray })
+      const docs = await reportsTransactions.bulk({ docs: reformattedTxs })
       let numErrors = 0
       for (const doc of docs) {
         if (doc.error != null) {
@@ -260,4 +247,32 @@ const standardizeNames = (field: string): string => {
     return CURRENCY_CONVERSION[field]
   }
   return field
+}
+
+async function standardTxReformat(
+  transaction: StandardTx,
+  appAndPluginId: string,
+  reportsTransactions: nano.DocumentScope<unknown>
+): Promise<DbTx> {
+  transaction.orderId = transaction.orderId.toLowerCase()
+  const key = `${appAndPluginId}:${transaction.orderId}`.toLowerCase()
+  const result = await reportsTransactions.get(key).catch(e => {
+    if (e != null && e.error === 'not_found') {
+      return {}
+    } else {
+      throw e
+    }
+  })
+  // no duplicate transactions
+  if (Object.keys(result).length > 0) {
+    throw new Error('duplicate transaction imported')
+  }
+  const newObj = { _rev: undefined, ...result, ...transaction, _id: key }
+
+  // replace all fields with non-standard names
+  newObj.depositCurrency = standardizeNames(newObj.depositCurrency)
+  newObj.payoutCurrency = standardizeNames(newObj.payoutCurrency)
+
+  datelog(`id: ${newObj._id}. revision: ${newObj._rev}`)
+  return newObj
 }
