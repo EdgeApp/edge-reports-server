cde3159ea3ddd4fbb13c71b0fd9c85afd983ba41
thomasgonda3 <passinto06@gmail.com>
2020-09-10 13:04:38 -0700

    Delete by Bulk

--- src/bin/destroyPartition.ts
+++ src/bin/destroyPartition.ts
@@ -1,56 +1,50 @@
 import nano from 'nano'
-import config from '../../config.json'
-import { asArray, asObject, asString } from 'cleaners'
+import js from 'jsonfile'
+import { asObject, asString } from 'cleaners'
 import { datelog } from '../util'
 
+const config = js.readFileSync('./config.json')
 const nanoDb = nano(config.couchDbFullpath)
 const reportsTransactions = nanoDb.use('reports_transactions')
 const reportsCache = nanoDb.use('reports_progresscache')
 
-const asDbReq = asObject({
-  docs: asArray(
-    asObject({
-      _id: asString,
-      _rev: asString
-    })
-  )
+interface Transactions {
+  _id: string
+  _rev: string
+  _deleted: boolean
+}
+
+const asPartitionedDoc = asObject({
+  id: asString,
+  value: asObject({ rev: asString })
 })
 
 async function main(partitionName: string): Promise<void> {
-  const query = {
-    selector: {
-      _id: { $exists: true }
-    },
-    fields: ['_id', '_rev'],
-    limit: 10000000
-  }
-  let result
+  const transactions: Transactions[] = []
   try {
-    const r = await reportsTransactions.partitionedFind(partitionName, query)
-    result = asDbReq(r)
+    await reportsTransactions.partitionedList(partitionName).then(body => {
+      body.rows.forEach(doc => {
+        asPartitionedDoc(doc)
+        transactions.push({
+          _id: doc.id,
+          _rev: doc.value.rev,
+          _deleted: true
+        })
+      })
+    })
   } catch (e) {
     datelog(e)
     throw e
   }
 
-  if (result.docs.length === 0) {
+  if (transactions.length === 0) {
     datelog(`Partition does not exist.`)
     return
   }
 
-  const promiseArray: Array<Promise<void>> = []
   try {
-    for (const tx of result.docs) {
-      promiseArray.push(
-        reportsTransactions
-          .destroy(tx._id, tx._rev)
-          .then(() =>
-            datelog(`Successfully Deleted: id:${tx._id}, rev: ${tx._rev}`)
-          )
-      )
-    }
-    await Promise.all(promiseArray)
-    datelog(`Successfully Deleted: ${result.docs.length} docs`)
+    await reportsTransactions.bulk({ docs: transactions })
+    datelog(`Successfully Deleted: ${transactions.length} docs`)
     datelog(`Successfully Deleted: partition ${partitionName}`)
 
     // Delete progress Cache
