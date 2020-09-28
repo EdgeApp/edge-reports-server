b8961b4c64f34f85b19fef1c30be5afcca785abf
thomasgonda3 <passinto06@gmail.com>
2020-09-28 16:21:07 -0700

    Obtain stats for all edge partitions at once
    (cherry picked from commit d525662a43b5c42f36431e14966a9391ac1cf04f)

--- src/bin/partitionStats.ts
+++ src/bin/partitionStats.ts
@@ -18,46 +18,77 @@ const nanoDb = nano(config.couchDbFullpath)
 const reportsTransactions = nanoDb.use('reports_transactions')
 
 async function main(partitionName: string): Promise<void> {
-  try {
-    const query = {
-      selector: {
-        _id: { $exists: true }
-      },
-      fields: ['timestamp', 'usdValue', 'rawTx'],
-      use_index: 'timestamp-index',
-      sort: ['timestamp'],
-      limit: 10000000000
-    }
-    const result = await reportsTransactions.partitionedFind(
-      partitionName,
-      query
+  const partitionNames: string[] = []
+  if (partitionName === 'all') {
+    partitionNames.push(
+      'edge_banxa',
+      'edge_bitaccess',
+      'edge_bitrefill',
+      'edge_bitsofgold',
+      'edge_bity',
+      'edge_changelly',
+      'edge_changenow',
+      'edge_coinswitch',
+      'edge_faast',
+      'edge_fox',
+      'edge_godex',
+      'edge_libertyx',
+      'edge_moonpay',
+      'edge_safello',
+      'edge_shapeshift',
+      'edge_simplex',
+      'edge_switchain',
+      'edge_totle',
+      'edge_transak',
+      'edge_wyre'
     )
-    const txs = asPartitionResult(result).docs
-    if (result.docs.length === 0) {
-      datelog(`Partition does not exist.`)
-      return
-    }
+  } else {
+    partitionNames.push(partitionName)
+  }
+  try {
+    for (const partner of partitionNames) {
+      const query = {
+        selector: {
+          _id: { $exists: true }
+        },
+        fields: ['timestamp', 'usdValue', 'rawTx'],
+        use_index: 'timestamp-index',
+        sort: ['timestamp'],
+        limit: 10000000000
+      }
+      const result = await reportsTransactions.partitionedFind(partner, query)
+      const txs = asPartitionResult(result).docs
+      if (result.docs.length === 0) {
+        datelog(`Partition does not exist.`)
+        return
+      }
 
-    const newestTx = new Date(
-      txs[txs.length - 1].timestamp * 1000
-    ).toISOString()
-    const oldestTx = new Date(txs[0].timestamp * 1000).toISOString()
-    let usdTxs = 0
-    let importedTxs = 0
-    for (let i = 0; i < txs.length; i++) {
-      if (typeof txs[i].usdValue !== 'undefined') {
-        usdTxs++
+      const newestTx = new Date(
+        txs[txs.length - 1].timestamp * 1000
+      ).toISOString()
+      const oldestTx = new Date(txs[0].timestamp * 1000).toISOString()
+      let usdTxs = 0
+      let usdAmount = 0
+      let importedTxs = 0
+      for (let i = 0; i < txs.length; i++) {
+        if (typeof txs[i].usdValue !== 'undefined') {
+          usdTxs++
+          usdAmount += txs[i].usdValue
+        }
+        if (typeof txs[i].rawTx === 'undefined') {
+          importedTxs++
+        }
       }
-      if (typeof txs[i].rawTx === 'undefined') {
-        importedTxs++
-      }
+      console.log(`${partner}:`)
+      console.log(`Total Number of Transactions: ${txs.length}`)
+      console.log(`Total Number of Transactions with usdValues: ${usdTxs}`)
+      console.log(`Total USD Value: ${usdAmount}`)
+      console.log(
+        `Total Number of Transactions that were imported: ${importedTxs}`
+      )
+      console.log(`Newest Transaction: ${newestTx}`)
+      console.log(`Oldest Transaction: ${oldestTx}`)
     }
-    datelog(`${partitionName}:`)
-    datelog(`Total Number of Transactions: ${txs.length}`)
-    datelog(`Total Number of Transactions with usdValues: ${usdTxs}`)
-    datelog(`Total Number of Transactions that were imported: ${importedTxs}`)
-    datelog(`Newest Transaction: ${newestTx}`)
-    datelog(`Oldest Transaction: ${oldestTx}`)
   } catch (e) {
     datelog(e)
     throw e
