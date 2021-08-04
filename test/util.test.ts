dde6e8bc661df2b3b817331385ecd4c3735e977c
Lex Peterson <>
2021-08-04 15:44:07 -0700

    Test movingAveDataSort util function

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,39 +1,15 @@
-import { expect } from 'chai'
+import { assert, expect } from 'chai'
 import { describe, it } from 'mocha'
 
-import { createQuarterBuckets } from '../src/util'
-
-const date = new Date(Date.now())
+import {
+  createQuarterBuckets,
+  movingAveDataSort,
+  sevenDayDataMerge
+} from '../lib/util'
+import { fixtures } from './utilFixtures.js'
 
 // add case with 1, 2, 3, 4 month bucket
-const fixtures = {
-  createQuarterBuckets: {
-    'Check empty month buckets': {
-      test: {
-        result: {
-          hour: [],
-          day: [],
-          month: [],
-          numAllTxs: 0
-        },
-        app: 'shsrth',
-        pluginId: 'erthe',
-        start: date.getTime() - 100000,
-        end: date.getTime()
-      },
-      expected: [
-        {
-          start: 0,
-          usdValue: 0,
-          numTxs: 0,
-          isoDate: date.toISOString(),
-          currencyCodes: {},
-          currencyPairs: {}
-        }
-      ]
-    }
-  }
-}
+
 const { createQuarterBuckets: quarterFixtures } = fixtures
 describe.skip('createQuarterBuckets', function() {
   for (const fixture in quarterFixtures) {
@@ -41,6 +17,38 @@ describe.skip('createQuarterBuckets', function() {
     it(`${fixture}`, function() {
       const result = createQuarterBuckets(test)
       expect(result).to.deep.equal(expected)
+    })
+  }
+})
+
+describe('movingAveDataSort', () => {
+  for (const testCase of fixtures.movingAveDataSort) {
+    it(testCase.testDescription, () => {
+      // Arrange
+      const inputData = testCase.inputData
+      const expectedType = testCase.outputType
+      const expectedOutput = testCase.expectedOutput
+      // Act
+      const actualResult = movingAveDataSort(inputData)
+      // Assert
+      assert.typeOf(actualResult, expectedType)
+      assert.deepEqual(actualResult, expectedOutput)
+    })
+  }
+})
+
+describe('sevenDayDataMerge', () => {
+  for (const testCase of fixtures.sevenDayDataMerge) {
+    it(testCase.testDescription, () => {
+      // Arrange
+      const inputData = testCase.inputData
+      const expectedType = testCase.outputType
+      const expectedOutput = testCase.expectedOutput
+      // Act
+      const actualResult = sevenDayDataMerge(inputData)
+      // Assert
+      assert.typeOf(actualResult, expectedType)
+      assert.deepEqual(actualResult, expectedOutput)
     })
   }
 })
