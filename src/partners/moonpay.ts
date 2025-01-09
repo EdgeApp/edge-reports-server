4731352c0dc377cd939ef51b50f1e755d0ed3e62
Sam Holmes <sam@edge.app>
2025-01-08 16:24:39 -0800

    Factor out `processMoonpayTx` function

--- src/partners/moonpay.ts
+++ src/partners/moonpay.ts
@@ -26,7 +26,7 @@ const asMoonpayTx = asObject({
 
 type MoonpayTx = ReturnType<typeof asMoonpayTx>
 
-const asMoonpayRawTx = asObject({
+const asPreMoonpayTx = asObject({
   status: asString
 })
 
@@ -38,7 +38,7 @@ const PER_REQUEST_LIMIT = 50
 export async function queryMoonpay(
   pluginParams: PluginParams
 ): Promise<PluginResult> {
-  const ssFormatTxs: StandardTx[] = []
+  const standardTxs: StandardTx[] = []
 
   let headers
   let latestTimestamp = 0
@@ -74,36 +74,12 @@ export async function queryMoonpay(
     // cryptoTransactionId is a duplicate among other transactions sometimes
     // in bulk update it throws an error for document update conflict because of this.
 
-    for (const rawtx of txs) {
-      if (asMoonpayRawTx(rawtx).status === 'completed') {
-        let tx: MoonpayTx
-        try {
-          tx = asMoonpayTx(rawtx)
-        } catch (e) {
-          datelog(e)
-          datelog(rawtx)
-          throw e
-        }
-
-        const date = new Date(tx.createdAt)
-        const timestamp = date.getTime()
-        const ssTx: StandardTx = {
-          status: 'complete',
-          orderId: tx.id,
-          depositTxid: undefined,
-          depositAddress: undefined,
-          depositCurrency: tx.baseCurrency.code.toUpperCase(),
-          depositAmount: tx.baseCurrencyAmount,
-          payoutTxid: tx.cryptoTransactionId,
-          payoutAddress: tx.walletAddress,
-          payoutCurrency: tx.currency.code.toUpperCase(),
-          payoutAmount: tx.quoteCurrencyAmount,
-          timestamp: timestamp / 1000,
-          isoDate: tx.createdAt,
-          usdValue: -1,
-          rawTx: rawtx
-        }
-        ssFormatTxs.push(ssTx)
+    for (const rawTx of txs) {
+      const preTx = asPreMoonpayTx(rawTx)
+      if (preTx.status === 'completed') {
+        const standardTx = processMoonpayTx(rawTx)
+        standardTxs.push(standardTx)
+        const timestamp = standardTx.timestamp * 1000
         done = latestTimestamp > timestamp || txs.length < PER_REQUEST_LIMIT
         newestTimestamp =
           newestTimestamp > timestamp ? newestTimestamp : timestamp
@@ -115,7 +91,7 @@ export async function queryMoonpay(
 
   const out: PluginResult = {
     settings: { latestTimestamp: newestTimestamp },
-    transactions: ssFormatTxs
+    transactions: standardTxs
   }
   return out
 }
@@ -126,4 +102,27 @@ export const moonpay: PartnerPlugin = {
   // results in a PluginResult
   pluginName: 'Moonpay',
   pluginId: 'moonpay'
+}
+
+export function processMoonpayTx(rawTx: unknown): StandardTx {
+  const tx: MoonpayTx = asMoonpayTx(rawTx)
+  const date = new Date(tx.createdAt)
+  const timestamp = date.getTime()
+  const standardTx: StandardTx = {
+    status: 'complete',
+    orderId: tx.id,
+    depositTxid: undefined,
+    depositAddress: undefined,
+    depositCurrency: tx.baseCurrency.code.toUpperCase(),
+    depositAmount: tx.baseCurrencyAmount,
+    payoutTxid: tx.cryptoTransactionId,
+    payoutAddress: tx.walletAddress,
+    payoutCurrency: tx.currency.code.toUpperCase(),
+    payoutAmount: tx.quoteCurrencyAmount,
+    timestamp: timestamp / 1000,
+    isoDate: tx.createdAt,
+    usdValue: -1,
+    rawTx
+  }
+  return standardTx
 }
