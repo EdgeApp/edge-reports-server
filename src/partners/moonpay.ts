2995ad9d82d351b746f18ffb4aa3441caa9399d3
Paul Puey <paul@edge.app>
2025-04-15 16:42:26 -0700

    Implement query of moonpay sell txs

--- src/partners/moonpay.ts
+++ src/partners/moonpay.ts
@@ -32,12 +32,23 @@ const asMoonpayTx = asObject({
   createdAt: asDate,
   id: asString,
   baseCurrencyId: asString,
-  currencyId: asString,
   currency: asMoonpayCurrency,
   baseCurrency: asMoonpayCurrency
 })
 
+const asMoonpaySellTx = asObject({
+  baseCurrencyAmount: asNumber,
+  quoteCurrencyAmount: asNumber,
+  createdAt: asDate,
+  id: asString,
+  baseCurrencyId: asString,
+  depositHash: asString,
+  quoteCurrency: asMoonpayCurrency,
+  baseCurrency: asMoonpayCurrency
+})
+
 type MoonpayTx = ReturnType<typeof asMoonpayTx>
+type MoonpaySellTx = ReturnType<typeof asMoonpaySellTx>
 
 const asMoonpayRawTx = asObject({
   status: asString
@@ -74,6 +85,58 @@ export async function queryMoonpay(
   const queryTimeStamp = new Date(latestIsoDate).getTime() - QUERY_LOOKBACK
   const queryIsoDate = new Date(queryTimeStamp).toISOString()
   let newestIsoDate = queryIsoDate
+
+  while (true) {
+    const url = `https://api.moonpay.io/v3/sell_transactions?limit=${PER_REQUEST_LIMIT}&offset=${offset}&startDate=${queryIsoDate}`
+    const result = await fetch(url, {
+      method: 'GET',
+      headers
+    })
+    const txs = asMoonpayResult(await result.json())
+
+    for (const rawtx of txs) {
+      if (asMoonpayRawTx(rawtx).status === 'completed') {
+        let tx: MoonpaySellTx
+        try {
+          tx = asMoonpaySellTx(rawtx)
+        } catch (e) {
+          datelog(e)
+          datelog(rawtx)
+          throw e
+        }
+
+        const isoDate = tx.createdAt.toISOString()
+        const timestamp = tx.createdAt.getTime()
+        const ssTx: StandardTx = {
+          status: 'complete',
+          orderId: tx.id,
+          depositTxid: tx.depositHash,
+          depositAddress: undefined,
+          depositCurrency: tx.baseCurrency.code.toUpperCase(),
+          depositAmount: tx.baseCurrencyAmount,
+          payoutTxid: undefined,
+          payoutAddress: undefined,
+          payoutCurrency: tx.quoteCurrency.code.toUpperCase(),
+          payoutAmount: tx.quoteCurrencyAmount,
+          timestamp: timestamp / 1000,
+          isoDate,
+          usdValue: -1,
+          rawTx: rawtx
+        }
+        ssFormatTxs.push(ssTx)
+        newestIsoDate = isoDate > newestIsoDate ? isoDate : newestIsoDate
+      }
+    }
+
+    if (txs.length < PER_REQUEST_LIMIT) {
+      break
+    }
+    console.log(
+      `Moonpay sell txs: ${JSON.stringify(txs[txs.length - 1]).slice(0, 100)}`
+    )
+
+    offset += PER_REQUEST_LIMIT
+  }
 
   while (true) {
     const url = `https://api.moonpay.io/v1/transactions?limit=${PER_REQUEST_LIMIT}&offset=${offset}&startDate=${queryIsoDate}`
@@ -123,6 +186,9 @@ export async function queryMoonpay(
       break
     }
 
+    console.log(
+      `Moonpay buy txs: ${JSON.stringify(txs[txs.length - 1]).slice(0, 100)}`
+    )
     offset += PER_REQUEST_LIMIT
   }
 
