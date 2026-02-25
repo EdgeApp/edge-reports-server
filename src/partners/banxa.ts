dfeb108717e6b932f64230dc711fa9167845d7d8
Paul Puey <paul@edge.app>
2026-02-25 13:59:36 -0800

    Add monad

--- src/partners/banxa.ts
+++ src/partners/banxa.ts
@@ -1,6 +1,8 @@
 import {
   asArray,
+  asEither,
   asMaybe,
+  asNull,
   asNumber,
   asObject,
   asOptional,
@@ -289,7 +291,7 @@ const asBanxaTx = asObject({
   id: asString,
   status: asBanxaStatus,
   created_at: asString,
-  country: asString,
+  country: asEither(asString, asNull),
   fiat_amount: asNumber,
   fiat_code: asString,
   coin_amount: asNumber,
